8d62eb596fbeafff18385581101e4b2c687999c0
William Ribeiro <williamrcestudos@gmail.com>
2025-12-27 14:59:12 -0300

    Upando o Projeto DevDojo

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+* Isso é um comentário <b>Javadoc</b>
+*/
+
+public class OlaDevDojo {
+    public static void main (String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
