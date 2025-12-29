099ce21d5f79f46092a4a1466d43ae51947f85ef
WilliamRC <williamrcestudos@gmail.com>
2025-12-29 15:30:00 -0300

    Upando projeto DevDojo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "William";
+        String endereco = "Rua São João";
+        double salario = 3789.39;
+        String dataRecebimentoSalario = "10/12/2025";
+        //String relatorio = "Eu "+nome+", morando no endereço " +endereco+  ", confirmo que recebi o salário de "+salario+ ", na data "+dataRecebimentoSalario+ ".";
+
+        System.out.println("Eu "+nome+", morando no endereço " +endereco+  ", confirmo que recebi o salário de "+salario+ ", na data "+dataRecebimentoSalario+ ".");
+
+    }
+}
