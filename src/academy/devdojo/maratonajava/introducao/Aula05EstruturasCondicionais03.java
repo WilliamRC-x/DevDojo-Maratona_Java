aa4c4404698aaedbbb4e345f1ed2e75d749949a0
WilliamRC <williamrcestudos@gmail.com>
2026-01-15 17:01:37 -0300

    Aula 25 - Switch

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -11,7 +11,7 @@ public class Aula05EstruturasCondicionais03 {
         String mensagemDoar = "Eu vou doar 500 pro DevDojo";
         String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
 
-        //                   (condicao)   ?  verdadeiro   :      falso
+        // (condicao) ? verdadeiro : falso
         String resultado = salario > 5000 ? mensagemDoar  : mensagemNaoDoar;
 
         System.out.println(resultado);
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        // Imprima o dia da semana, considerando 1 como domingo
+
+        byte dia = 5;
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo"); break;
+                case 1:
+                System.out.println("Domingo"); break;case 1:
+                System.out.println("Domingo"); break;case 1:
+                System.out.println("Domingo"); break;case 1:
+                System.out.println("Domingo"); break;
+
+        }
+    }
+}
