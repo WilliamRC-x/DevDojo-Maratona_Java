44aa3acf9bb079f6ea9d7a3ed4b30e5ff340608e
WilliamRC <williamrcestudos@gmail.com>
2026-03-23 12:21:23 -0300

    Array Foreach

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays03Foreach.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03Foreach.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula07Arrays03 {
+public class Aula07Arrays03Foreach {
     public static void main(String[] args) {
         int[] numeros = new int[5];
         int[] numeros2 = {1, 2, 3, 4, 5};
